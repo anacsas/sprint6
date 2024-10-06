9841009cc61aeb7256417f8e4e23e52516c67db4
anacsas <anakarencasas17@gmail.com>
2024-10-06 13:35:07 -0700

    Update for corrections

--- app.py
+++ app.py
@@ -3,6 +3,11 @@ import plotly.express as px
 import streamlit as st
         
 car_data = pd.read_csv('notebooks/vehicles_us.csv') # leer los datos
+
+# Encabezado de la aplicación
+st.title('Análisis de Datos de Vehículos')
+st.header('Exploración de Datos de Anuncios de Venta de Coches')
+
 hist_button = st.button('Construir histograma') # crear un botón
         
 if hist_button: # al hacer clic en el botón
@@ -19,3 +24,14 @@ build_histogram = st.checkbox('Construir un histograma')
 
 if build_histogram: # si la casilla de verificación está seleccionada
     st.write('Construir un histograma para la columna odómetro')
+
+scatter_button = st.button('Construir scatterplot')
+
+if scatter_button:  # Al hacer clic en el botón
+    st.write('Creación de un scatterplot para el conjunto de datos')
+    
+    # Crear un scatterplot
+    fig_scatter = px.scatter(car_data, x="odometer", y="price", title="Scatterplot de Odometer vs Price")
+    
+    # Mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig_scatter, use_container_width=True)
